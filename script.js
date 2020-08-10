6ca1b7e9565a79bdb88a27c70d2291677f2b83d7
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-10 00:26:08 -0700

    Updating format and style

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
+//Setting element variables
+var startButton = document.querySelector("start-quiz"),
+
 //Array containing questions, answer options, and correct answer
 var questions = [
   {
     question: "Which of the following is not a type of loop?",
-    options: [],
-    answer: "",
+    options: ["1", "2", "3", "4"],
+    answer: 2 || "2",
   },
   {
-    question: "What color is my dog?",
-    options: [],
-    answer: "",
+    question: "What does DOM stand for?",
+    options: ["1", "2", "3", "4"],
+    answer: 2 || "2",
   },
   {
     question: "What do you mean I don't have a dog?",
-    options: [],
-    answer: "",
+    options: ["1", "2", "3", "4"],
+    answer: 2 || "2",
   },
   {
     question: "Why did the chicken cross the road?",
-    options: [],
-    answer: "",
+    options: ["1", "2", "3", "4"],
+    answer: 2 || "2",
   },
   {
     question: "Is abba-zabba truly you're only friend?",
-    options: [],
-    answer: "",
+    options: ["1", "2", "3", "4"],
+    answer: 2 || "2",
   },
   {
-    question: "How much time is left on the timer?",
-    options: [],
-    answer: "",
+    question: "What does CSS stand for?",
+    options: ["1", "2", "3", "4"],
+    answer: 2 || "2",
   },
   {
     question: "What happens after this last question?",
-    options: [],
-    answer: "",
+    options: ["1", "2", "3", "4"],
+    answer: 2 || "2",
   },
 ];
 
@@ -42,3 +45,6 @@ var score = 0;
 var questionDisplay = 0;
 var initialTime = 75;
 
+function setTimer() {
+  var 
+}
