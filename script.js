3b5d2f5071febbda39758aae0daa0b5a1ae45928
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-09 21:52:59 -0700

    creating question array and variables for high score and timer

---
+++ script.js
@@ -0,0 +1,44 @@
+//Array containing questions, answer options, and correct answer
+var questions = [
+  {
+    question: "Which of the following is not a type of loop?",
+    options: [],
+    answer: "",
+  },
+  {
+    question: "What color is my dog?",
+    options: [],
+    answer: "",
+  },
+  {
+    question: "What do you mean I don't have a dog?",
+    options: [],
+    answer: "",
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    options: [],
+    answer: "",
+  },
+  {
+    question: "Is abba-zabba truly you're only friend?",
+    options: [],
+    answer: "",
+  },
+  {
+    question: "How much time is left on the timer?",
+    options: [],
+    answer: "",
+  },
+  {
+    question: "What happens after this last question?",
+    options: [],
+    answer: "",
+  },
+];
+
+//declaring variables
+var score = 0;
+var questionDisplay = 0;
+var initialTime = 75;
+
