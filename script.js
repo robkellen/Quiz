961e8217cf096dc1cdfd458796638423f7c88db1
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-11 00:47:33 -0700

    adding functions

--- script.js
+++ script.js
@@ -44,25 +44,58 @@ var questionIndex = 0;
 //setting variables for manipulation
 var timer = document.getElementById("timer");
 var startQuiz = document.getElementById("startQuiz");
-var questionsDiv = document.getElementById("mainDiv");
+var mainDiv = document.getElementById("mainDiv");
 var timeRemaining = 76;
 var interval = 0;
 var timePenalty = 10;
-var newUL = document.createElement("ul");
+var createNewUL = document.createElement("ul");
 
 //add event listener for timer on click of start button
-startQuiz.addEventListener("click", function(e) {
-  console.log(e);
-  var timeRemaining = 76;
-  timer = setInterval(function () {
-    timeRemaining--;
-    timer.textContent = "Time" + timeRemaining;
+startQuiz.addEventListener("click", function (e) {
+  if (interval === 0) {
+    interval = setInterval(function () {
+      timeRemaining--;
+      timer.textContent = "Time: " + timeRemaining;
 
-    if (timeRemaining === 0) {
-      clearInterval(interval);
-      gameOver();
-
-    }
-  }, 1000)
+      if (timeRemaining === 0) {
+        clearInterval(interval);
+        gameOver();
+        timer.textContent = "Out of Time!";
+      }
+    }, 1000);
+  }
+  renderQuestions(questionIndex);
 });
+//renders questions and answer options to the HTML
+function renderQuestions(questionIndex) {
+  //clearing intro
+  mainDiv.innerHTML = "";
+  createNewUL.innerHTML = "";
+  // for loop to add text from questions array
+  for (let i = 0; i < questions.length; i++) {
+    var questionTitle = questions[questionIndex].question;
+    var answerOption = questions[questionIndex].answer;
+    mainDiv.textContent = questionTitle;
+  }
 
+  //to create list of options and append corresponding answer choices for each question
+  answerOption.forEach(function (newItem) {
+    var answerItem = document.createElement("li");
+    answerItem.textContent = newItem;
+    mainDiv.appendChild(createNewUL);
+    createNewUL.appendChild(answerItem);
+    answerItem.addEventListener("click", (compare));
+  })
+}
+
+//create compare function
+
+
+//determine if last question has been answered
+
+
+//create game over function
+
+//tally score
+
+//create input for initials to store to local storage
